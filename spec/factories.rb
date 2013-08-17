6c7dadbbd89d0e8c90a80b82c42cebcd27dbc39e
Matt Adelman <madelman@wesleyan.edu>
2013-08-17 13:01:12 -0400

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name     "Matt Adelman"
-    email    "madelman@yahoo.com"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
     password "foobar"
     password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
   end
 end
